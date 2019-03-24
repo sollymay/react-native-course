3ffb77226e085712ba560db0972800649a33ecbb
Salomon May <sollymay@gmail.com>
2019-03-24 16:16:51 -0400

    added changes to header

--- albums/index.js
+++ albums/index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
-AppRegistry.registerComponent(appName, () => App);
+// Create a component
+const App = () => (
+        <Header headerText={'Albums'} />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import { name as appName } from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
